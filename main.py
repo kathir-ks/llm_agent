65777dc8167d16ca12dd2001f003875f7e74424b
kathir <128504080+kathir-ks@users.noreply.github.com>
2023-12-18 01:59:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from agent import Agent
 from groupchat import GroupChat
 
-GOOGLE_API_KEY = "AIzaSyCc4o3_ZNbpwLGisP1SRw_hQaWTzo0wavE"
+GOOGLE_API_KEY = ""
 
 genai.configure(api_key = GOOGLE_API_KEY)
 
